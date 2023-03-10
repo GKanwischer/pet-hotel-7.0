ed842c7ac1cb114732deeb99a9b85d46412ce92b
masonleonhart <mason.leonhart@gmail.com>
2023-03-10 15:37:24 -0600

    assignment ready

--- Program.cs
+++ Program.cs
@@ -1,8 +1,18 @@
+using Microsoft.EntityFrameworkCore;
+using pet_hotel.Models;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllersWithViews();
+
+string DATABASE_URL = Environment.GetEnvironmentVariable("DATABASE_URL_STR");
+string connectionString = (DATABASE_URL == null ? builder.Configuration.GetConnectionString("DefaultConnection") : DATABASE_URL);
+Console.WriteLine($"Using connection string: {connectionString}");
+
+builder.Services.AddDbContext<ApplicationContext>(options =>
+    options.UseNpgsql(connectionString)
+);
 
 var app = builder.Build();
 
